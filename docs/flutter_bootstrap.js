a13fb398df66e0f98b9b67caeaa6cb4231a83e36
Mansi Khatri <97281174+MansiAtKody@users.noreply.github.com>
2024-08-31 22:59:03 +0530

    doc change

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2873230667"
+    serviceWorkerVersion: "1613073912"
   }
 });
